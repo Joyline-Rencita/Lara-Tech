fe109db73dc9186737af1c0817ac2adb317ac5ba
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-26 14:32:53 +0530

    Update and rename 01_Attributes POs.sql to 01_Create Purchase Orders.sql

---
+++ week-6/OCPM/Events/01_Create Purchase Orders.sql
@@ -0,0 +1,31 @@
+  ********************************  POPULATING THE EVENT ATTRIBUTES  *************************************
+	  
+SELECT
+    "o_custom_JoylineEKKO"."ERNAM" AS "user",
+    "o_custom_JoylineEKKO"."ID" AS "ID",
+    "o_custom_JoylineEKKO"."AEDAT" AS "Time",
+    "o_custom_JoylineEKKO"."BUKRS" as "CompanyCode",
+    "o_custom_JoylineEKKO"."BSART" AS "PurchasingDocType",
+    "o_custom_JoylineEKKO"."BSTYP" AS "PurchasingDocCategory",
+    "o_custom_JoylineEKKO"."EKORGTEXT" AS "PurchasingOrg",
+    "o_custom_JoylineEKKO"."WAERS" AS "CurrencyKey",
+    "o_custom_JoylineEKKO"."WKURS" AS "ExchangeRate",
+    "o_custom_JoylineEKKO"."EKGRP" AS "PurchasingGroup",
+    "o_custom_JoylineEKKO"."ORDEREDDATE" AS "OrderDate",
+    "o_custom_JoylineEKKO"."SUBMITDATE" AS "SubmittedDate",
+    NULL AS JoylineEKKO
+FROM "o_custom_JoylineEKKO"
+	LEFT JOIN "c_o_custom_JoylineEKKO" AS EKKO
+	ON EKKO.OBJECTID = "o_custom_JoylineEKKO"."ID"
+WHERE "o_custom_JoylineEKKO"."BSTYP" = 'F'
+
+
+  ********************************  POPULATING THE EVENT RELATIONSHIPS  *************************************
+
+SELECT
+	"o_custom_JoylineEKKO"."ID" AS "ID",
+	"o_custom_JoylineEKPO"."ID" AS "JoylineEKPO"
+FROM "o_custom_JoylineEKKO"
+	LEFT JOIN "o_custom_JoylineEKPO"
+	ON "o_custom_JoylineEKKO"."ID" = "o_custom_JoylineEKPO"."EBELP"
+WHERE "o_custom_JoylineEKKO"."BSTYP" = 'F'
