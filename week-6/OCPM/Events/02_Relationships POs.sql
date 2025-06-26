1e7776851166100e3e63bb63030b2650e471c977
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-26 14:27:21 +0530

    Delete week-6/OCPM/Events/02_Relationships POs.sql

--- week-6/OCPM/Events/02_Relationships POs.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT
-	"o_custom_JoylineEKKO"."ID" AS "ID",
-	"o_custom_JoylineEKPO"."ID" AS "JoylineEKPO"
-FROM "o_custom_JoylineEKKO"
-	LEFT JOIN "o_custom_JoylineEKPO"
-	ON "o_custom_JoylineEKKO"."ID" = "o_custom_JoylineEKPO"."EBELP"
--- WHERE <>
