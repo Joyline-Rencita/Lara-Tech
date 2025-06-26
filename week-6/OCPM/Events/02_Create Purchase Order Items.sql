0f4a06d1c9be3c838de2f3197ab2b0c5dcb223f7
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-26 15:44:15 +0530

    Update 02_Create Purchase Order Items.sql

--- week-6/OCPM/Events/02_Create Purchase Order Items.sql
+++ week-6/OCPM/Events/02_Create Purchase Order Items.sql
@@ -1,18 +1,23 @@
-  ********************************  POPULATING THE EVENT ATTRIBUTES  *************************************
+
+********************************  POPULATING THE EVENT ATTRIBUTES  *************************************
 
 SELECT
-	"o_custom_JoylineEKPO"."ID" AS "ID",
-	"o_custom_JoylineEKPO"."AEDAT" AS "Time",
+    "o_custom_JoylineEKPO"."ID" AS "ID",
+    "o_custom_JoylineEKPO"."AEDAT" AS "Time",
     "o_custom_JoylineEKPO"."BUKRS" AS "companyCode",
     "o_custom_JoylineEKPO"."MATNR" AS "materialNum",
     "o_custom_JoylineEKPO"."MATKL" AS "materialGroupNum",
     "o_custom_JoylineEKPO"."NetwrConverted" AS "netValue",
     "o_custom_JoylineEKPO"."NetprConverted" AS "netPrice",
+    "EKPO"."OldValue" AS "OldValue",     -- Values are not fetched 
+    "EKPO"."NewValue" AS "NewValue",     -- Values are not fetched 
+    "EKPO"."ChangedBy" AS "UserName",    -- Values are not fetched 
     NULL AS JoylineEKKO
 
 FROM "o_custom_JoylineEKPO"
 	LEFT JOIN "c_o_custom_JoylineEKPO" AS EKPO
 	ON EKPO.OBJECTID = "o_custom_JoylineEKPO"."ID"
+-- WHERE <>
 
 
 ********************************  POPULATING THE EVENT RELATIONSHIPS  *************************************
