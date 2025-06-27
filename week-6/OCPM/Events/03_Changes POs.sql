9677cc7b0782cd0556bc5a582fb7e3cd5804a0b7
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-27 08:25:37 +0530

    Update 03_Changes POs.sql

--- week-6/OCPM/Events/03_Changes POs.sql
+++ week-6/OCPM/Events/03_Changes POs.sql
@@ -15,3 +15,13 @@ FROM "o_custom_JoylineEKKO"
 
 
 ********************************  POPULATING THE EVENT RELATIONSHIPS  *************************************
+
+SELECT
+	"o_custom_JoylineEKKO"."ID" AS "ID",
+	"o_custom_JoylineEKPO"."ID" AS "JoylineEKPO"
+    -- "c_o_custom_JoylineEKPO"."Time" AS "TIME"
+FROM "o_custom_JoylineEKKO"
+	LEFT JOIN "o_custom_JoylineEKPO"
+	ON "o_custom_JoylineEKKO"."ID" = "o_custom_JoylineEKPO"."EBELP"
+LEFT JOIN "c_o_custom_JoylineEKPO"                                         ---   JOINED CDPOS AND EKPO
+    ON  "c_o_custom_JoylineEKPO"."ObjectID" = "o_custom_JoylineEKPO"."ID"
