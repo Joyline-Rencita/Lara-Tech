4dc4287dd985d0d81e40d6dfb6ff25152e46be29
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-27 08:51:28 +0530

    Update 03_Changes POs.sql

--- week-6/OCPM/Events/03_Changes POs.sql
+++ week-6/OCPM/Events/03_Changes POs.sql
@@ -12,7 +12,29 @@ FROM "o_custom_JoylineEKKO"
 	LEFT JOIN "c_o_custom_JoylineEKKO" AS EKKO
 	ON EKKO.OBJECTID = "o_custom_JoylineEKKO"."ID"
 -- WHERE 
-
+/*
+CASE 
+		WHEN CHANGES.FNAME = 'LOEKZ' AND CHANGES.VALUE_NEW ='S' THEN 'Block Purchase Order Item'
+		WHEN CHANGES.FNAME = 'LOEKZ' AND CHANGES.VALUE_OLD ='S' AND CHANGES.VALUE_NEW IS NULL THEN 'Reactivate Purchase Order Item'
+		WHEN CHANGES.FNAME = 'LOEKZ' AND CHANGES.VALUE_NEW ='L' THEN 'Delete Purchase Order Item'
+		WHEN CHANGES.FNAME = 'LOEKZ' AND CHANGES.VALUE_OLD ='L' AND CHANGES.VALUE_NEW IS NULL THEN 'Reactivate Purchase Order Item'
+		WHEN CHANGES.FNAME = 'LOEKZ' THEN 'Change Deletion Flag for Purchase Order Item'
+		WHEN CHANGES.FNAME = 'NETPR' THEN 'Change Price'
+		WHEN CHANGES.FNAME = 'MENGE' THEN 'Change Quantity'
+		WHEN CHANGES.FNAME = 'LGORT' THEN 'Change Storage Location'
+		WHEN CHANGES.FNAME = 'LABNR' AND CHANGES.VALUE_OLD IS NULL THEN 'Receive Order Confirmation'
+		WHEN CHANGES.FNAME = 'LABNR' THEN 'Update Order Confirmation'
+		WHEN CHANGES.FNAME = 'ELIKZ' THEN 'Change Delivery Indicator'
+		WHEN CHANGES.FNAME = 'EREKZ' THEN 'Change Final Invoice Indicator'
+		WHEN CHANGES.FNAME = 'EGLKZ' THEN 'Change Outward Delivery Indicator'
+		WHEN CHANGES.FNAME = 'ABSKZ' THEN 'Change Rejection Indicator'
+        	WHEN CHANGES.FNAME = 'KONNR' THEN 'Change Contract'
+		WHEN CHANGES.FNAME = 'KTPNR' THEN 'Change Contract'
+        	WHEN CHANGES.FNAME = 'TXZ01' THEN 'Change Short Text Description'
+       		WHEN CHANGES.FNAME = 'MATNR' THEN 'Change Material Number'
+        	WHEN CHANGES.FNAME = 'MATKL' THEN 'Change Material Group'
+	END	
+*/
 
 ********************************  POPULATING THE EVENT RELATIONSHIPS  *************************************
 
