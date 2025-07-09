cea2b3a3c0eda26b2ac6fc7428c231ccda9610f6
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-09 20:12:37 +0530

    Update 06_Clear Invoice.sql

--- week-6/OCPM/Events/06_Clear Invoice.sql
+++ week-6/OCPM/Events/06_Clear Invoice.sql
@@ -1,4 +1,13 @@
   ********************************    POPULATING THE EVENT ATTRIBUTES    *************************************
-
+   
+SELECT
+	"o_custom_JoylineRSEG"."ID" AS "ID",
+	"o_custom_JoylineBKPF"."AEDAT"AS "Time",
+	-- "o_custom_JoylineBKPF"."ID" AS "JoylineBKPF"
+    NULL AS "JoylineBKPF"
+FROM "o_custom_JoylineRSEG"
+	LEFT JOIN "o_custom_JoylineBKPF"
+	ON "o_custom_JoylineBKPF"."GJAHR" = "o_custom_JoylineRSEG"."GJAHR"
+-- WHERE <>
 
   ********************************  POPULATING THE EVENT RELATIONSHIPS  *************************************
