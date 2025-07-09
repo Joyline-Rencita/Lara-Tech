5e5d4595f0514eb4dd002e428f6eec462215bba4
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-09 22:31:49 +0530

    Update 05_Create Invoice.sql

--- week-6/OCPM/Events/05_Create Invoice.sql
+++ week-6/OCPM/Events/05_Create Invoice.sql
@@ -1,15 +1,19 @@
 
 ********************************************     POPULATING THE EVENT ATTRIBUTES      *****************************************
 
+
 SELECT
-	"o_custom_JoylineRSEG"."ID" AS "ID",
-	"o_custom_JoylineLFA1"."ERDAT" AS "Time",
-        "o_custom_JoylineRSEG"."JoylineEKPORSEG_ID" AS "JoylineLFA1"
+    "o_custom_JoylineRSEG"."ID" AS "ID",
+    "o_custom_JoylineLFA1"."ERDAT" AS "Time",
+    "o_custom_JoylineRSEG"."MANDT" AS "MANDT",
+    "o_custom_JoylineRSEG"."EbelpEkpo" AS "EBELP",
+    "o_custom_JoylineRSEG"."GJAHR" as "FiscalYear",
+    "o_custom_JoylineRSEG"."NetwrEkpo" AS "NetOrderValue",
+    "o_custom_JoylineRSEG"."NetprEkpo" AS "NetPrice",
+    "o_custom_JoylineRSEG"."JoylineEKPORSEG_ID" AS "JoylineLFA1"
 FROM "o_custom_JoylineRSEG"
 	LEFT JOIN "o_custom_JoylineLFA1"
 	ON "o_custom_JoylineLFA1"."ID" = "o_custom_JoylineRSEG"."LifnrEkko"
 -- WHERE <>
-
-
 
 ********************************************     POPULATING THE EVENT RELATIONSHIPS    *****************************************
