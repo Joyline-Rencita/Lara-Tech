6aca39029ab9c6f8d1181efd8ebdd073bc04496f
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-09 19:39:47 +0530

    Create 05_RSEG.sql

---
+++ week-6/OCPM/Objects/05_RSEG.sql
@@ -0,0 +1,32 @@
+********************************    POPULATING THE OBJECTATTRIBUTES    ****************************	
+
+SELECT
+	"_CASE_KEY" AS "ID",
+	"MANDT" AS "MANDT",
+	"BELNR" AS "BELNR",
+	"GJAHR" AS "GJAHR",
+	"BUZEI" AS "BUZEI",
+	"LFBNR" AS "LFBNR",
+	"LFGJA" AS "LFGJA",
+	"LFPOS" AS "LFPOS",
+	"SHKZG" AS "SHKZG",
+	"SPGRM" AS "SPGRM",
+	"WRBTR" AS "WRBTR",
+	"MENGE" AS "MENGE",
+	"EBELP_EKPO" AS "EbelpEkpo",
+	"BUKRS_EKPO" AS "BukrsEkpo",
+	"NETPR_EKPO" AS "NetprEkpo",
+	"NETWR_EKPO" AS "NetwrEkpo",
+	"MATNR_EKPO" AS "MatnrEkpo",
+	"WERKS_EKPO" AS "WerksEkpo",
+	"ZTERM_EKKO" AS "ZtermEkko",
+	"WAERS_EKKO" AS "WaersEkko",
+	"LIFNR_EKKO" AS "LifnrEkko",
+	"BEDAT_EKKO" AS "BedatEkko",
+	"EBELN_EKPO" AS "JoylineEKPORSEG"
+FROM "RSEG"
+
+
+******************************   POPULATING THE OBJECT CHANGES   *******************************
+
+
