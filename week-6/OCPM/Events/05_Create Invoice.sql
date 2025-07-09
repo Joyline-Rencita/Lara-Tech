c3c4c15b67232d213b3ac3165a46c2c2a2eb152a
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-09 19:43:33 +0530

    Update 05_Create Invoice.sql

--- week-6/OCPM/Events/05_Create Invoice.sql
+++ week-6/OCPM/Events/05_Create Invoice.sql
@@ -4,7 +4,7 @@
 SELECT
 	"o_custom_JoylineRSEG"."ID" AS "ID",
 	"o_custom_JoylineLFA1"."ERDAT" AS "Time",
-    "o_custom_JoylineRSEG"."JoylineEKPORSEG_ID" AS "JoylineLFA1"
+        "o_custom_JoylineRSEG"."JoylineEKPORSEG_ID" AS "JoylineLFA1"
 FROM "o_custom_JoylineRSEG"
 	LEFT JOIN "o_custom_JoylineLFA1"
 	ON "o_custom_JoylineLFA1"."ID" = "o_custom_JoylineRSEG"."LifnrEkko"
