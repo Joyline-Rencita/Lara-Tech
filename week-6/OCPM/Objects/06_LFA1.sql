4e44f3ca5fc2dbba7470a69190215cc5ef3a3a3d
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-09 19:42:17 +0530

    Create 06_LFA1.sql

---
+++ week-6/OCPM/Objects/06_LFA1.sql
@@ -0,0 +1,14 @@
+********************************    POPULATING THE OBJECTATTRIBUTES     ****************************	
+
+SELECT
+	TRY_CAST("LIFNR" AS VARCHAR(255)) AS "ID",
+	"MANDT" AS "MANDT",
+	"LAND1" AS "LAND1",
+	"NAME1" AS "NAME1",
+	"ERDAT" AS "ERDAT",
+	"ERNAM" AS "ERNAM"
+FROM "LFA1"
+
+
+*********************************     POPULATING THE OBJECT CHANGES     *********************************	
+
